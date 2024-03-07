abb45e1c71409f195ab1d25d20bd548f12291e0c
Clément Joly <foss@131719.xyz>
2024-03-07 07:20:04 +0000

    Add script to automate some of the update process
    The testing is still done manually against my website

---
+++ update.fish
@@ -0,0 +1,16 @@
+#!/bin/env fish
+
+set -l aversion $argv[1]
+
+if test -z $aversion
+    echo "No version specified"
+    exit 1
+end
+
+wget -O assets/js/asciinema-player.min.js https://github.com/asciinema/asciinema-player/releases/download/v$aversion/asciinema-player.min.js
+wget -O assets/css/asciinema-player.css https://github.com/asciinema/asciinema-player/releases/download/v$aversion/asciinema-player.css
+
+git cr "Update to $aversion, downloaded from Release page"
+git tag -s v$aversion -m "Version $aversion"
+
+git show v$aversion
